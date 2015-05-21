31005f9811fbcaf9fe81306d8b2a73b59d44328e
Petri Mattila <petri_mattila@xyratex.com>
2015-05-21 10:43:32 +0100

    Change number of entities in companion

--- companion/src/firmwares/opentx/opentxinterface.cpp
+++ companion/src/firmwares/opentx/opentxinterface.cpp
@@ -106,7 +106,7 @@ const int OpenTxEepromInterface::getMaxModels()
   else if (board == BOARD_GRUVIN9X)
     return 30;
   else
-    return 16;
+    return 7;
 }
 
 template <class T>
@@ -528,7 +528,7 @@ int OpenTxFirmware::getCapability(const Capability capability)
       else if (board==BOARD_GRUVIN9X)
         return 6;
       else
-        return 5;
+        return 3;
     case FlightModesHaveFades:
       return 1;
     case Heli:
